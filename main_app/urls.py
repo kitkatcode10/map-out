564b262d70607d20fb087f11998c3a207b5e178b
Katherine Wong <katherinewongkw3@gmail.com>
2021-06-29 16:21:12 -0700

    update create function

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('vacations/create/', views.VacationCreate.as_view(), name='vacations_create'), 
     path('vacations/<int:pk>/update/', views.VacationUpdate.as_view(), name='vacations_update'),
 ]
+
