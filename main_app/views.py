530545a40d4e92eca2b819e59cd8f0acb14bc754
Katherine Wong <katherinewongkw3@gmail.com>
2021-06-30 22:45:44 -0700

    troubleshoot submit vacation form

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, redirect
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.views.generic import ListView, DetailView
