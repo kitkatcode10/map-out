3d144d5d4bf060f0519f81eca8c0254e813eb3a5
NRaeewal <39168910+NRaeewal@users.noreply.github.com>
2021-07-03 06:20:52 -0600

    fixed photo model

--- main_app/views.py
+++ main_app/views.py
@@ -10,8 +10,14 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Vacation, Packing 
+import uuid
+import boto3
+from .models import Vacation, Packing, Photo
 from .forms import ItineraryForm
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'vacaycollector'
+
 # Create your views here.
 def home(request):
     return redirect ('about')
@@ -107,3 +113,23 @@ class PackingList(ListView):
 class PackingUpdate(UpdateView):
   model= Packing
   fields = '__all__'
+
+
+def add_photo(request, vacation_id):
+	# photo-file was the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" for S3 / needs image file extension too
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    # just in case something goes wrong
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      # build the full url string
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      # we can assign to cat_id or cat (if you have a cat object)
+      photo = Photo(url=url, vacation_id=vacation_id)
+      photo.save()
+    except:
+      print('An error occurred uploading file to S3')
+  return redirect('detail', vacation_id=vacation_id)
