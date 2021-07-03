f299c2f27a42c5ace96f9203836bb2b5b9671ba5
Katherine Wong <katherinewongkw3@gmail.com>
2021-07-03 15:39:13 -0700

    config deploy pt 3

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn map-out.wsgi
+web: gunicorn map_out.wsgi
--- mapout/settings.py
+++ mapout/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,9 +129,9 @@ LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # heroku
-import django_heroku
+
 django_heroku.settings(locals())
 
 
-WSGI_APPLICATION = 'map-out.wsgi.application'
+WSGI_APPLICATION = 'map_out.wsgi.application'
 # catcollector is the project name
